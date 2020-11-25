0be09da2ba490838a07ff8082ac7127c3e07d3a2
Abdullah03366 <abdullah.sanli@student.hu.nl>
2020-11-25 23:14:32 +0100

    Code aangepast (scrollbar toegevoegd, GUI uitgebreid met buttons, datastructuur)

--- AI/AI-sprint 1.py
+++ AI/AI-sprint 1.py
@@ -4,35 +4,62 @@ import requests
 github_example = "https://raw.githubusercontent.com/AlphaConsultants/ALPHA/main/AI/textdocumenttest.json"
 json_data = requests.get(github_example).json()
 
-print(json_data)
 
-def toon_eerste_spel():
-    eerste_spel_frame.pack(side=LEFT)
-    for games in json_data:
-        if games["appid"] < 20:
-            eerste_spel = games["name"], "\n", games["release_date"], "\n", games["price"]
-            spel_label = Label(eerste_spel_frame, text=eerste_spel, bg="red", fg="white", font=("Calibri", 18, "bold"))
-            spel_label.pack(padx=200, pady=20)
+games_list = []
+
+for data in json_data:
+    rating_in_percentage = ((data["positive_ratings"] - data["negative_ratings"]) / data["positive_ratings"]) * 100
+    rating_in_percentage_afgerond = str(round(rating_in_percentage, 1))
+    data_per_game = data["appid"], data["name"], data["genres"], data["release_date"], data["price"], data["owners"], rating_in_percentage_afgerond, data["required_age"], "\n"
+    games_list.append(tuple(data_per_game))
+
+print(games_list)
+
+
+def toon_spellen():
+    data_box.pack()
+    positie = 0
+    for games in games_list:
+        for game in games:
+            positie += 1
+            data_box.insert(positie, game)
+
+
+# def toon_eerste_spel():
+#     eerste_spel_frame.pack(side=LEFT)
+#     for games in json_data:
+#         if games["appid"] < 20:
+#             eerste_spel = games["name"], "\n", games["release_date"], "\n", games["price"]
+#             spel_label = Label(eerste_spel_frame, text=eerste_spel, bg="red", fg="white", font=("Calibri", 18, "bold"))
+#             spel_label.pack(padx=200, pady=20)
 
 
 root = Tk()
 root.title("Dashboard Steam")
 root.geometry("1800x1000")
 root.configure(bg="#17202e")
-SorteerFrame = Frame(root, bg="#17202e")
-LinkerFrame = Frame(SorteerFrame, bg="#17202e")
-RechterFrame = Frame(SorteerFrame, bg="#17202e")
+
+
+'''Frames voor widgets'''
+DataFrame = Frame(root, bg="#17202e")
+SorteerButtonFrame = Frame(root, bg="#17202e")
+LinkerFrame = Frame(SorteerButtonFrame, bg="#17202e")
+RechterFrame = Frame(SorteerButtonFrame, bg="#17202e")
 eerste_spel_frame = Frame(root, bg="#17202e")
 
-titel = Label(root, text="Dashboard Steam", bg="#17202e", fg="white", font=("Calibri", 40, "bold", "underline"))
+
+'''Titel van dashboard'''
+titel = Label(root, text="Alpha Consultants", bg="#17202e", fg="white", font=("Calibri", 40, "bold", "underline"))
 titel.pack(pady=90)
 
-SorteerFrame.pack(side=RIGHT)
+DataFrame.place(x=100, y=400)
+SorteerButtonFrame.pack(side=RIGHT)
 LinkerFrame.pack(side=RIGHT, pady=55, padx=30)
 RechterFrame.pack(side=RIGHT, pady=55, padx=30)
 
 
-SorteerTitel = Label(SorteerFrame, text="Sorteren op:", bg="#17202e", fg="white", font=("Calibri", 24, "bold"))
+'''Sorteerknoppen met titel'''
+SorteerTitel = Label(SorteerButtonFrame, text="Sorteren op:", bg="#17202e", fg="white", font=("Calibri", 24, "bold"))
 Name_AZ = Button(LinkerFrame, text="Naam (A-Z)", width=24, font=("Calibri", 14, "bold"))
 Name_ZA = Button(RechterFrame, text="Naam (Z-A)", width=24, font=("Calibri", 14, "bold"))
 Genre_AZ = Button(LinkerFrame, text="Genre (A-Z)", width=24, font=("Calibri", 14, "bold"))
@@ -64,5 +91,33 @@ Rating_HL.pack(pady=10)
 RequiredAge_LH.pack(pady=10)
 RequiredAge_HL.pack(pady=10)
 
-toon_eerste_spel()
+
+'''Scrollbar'''
+sb = Scrollbar(DataFrame)
+sb.pack(side=RIGHT, fill=Y)
+
+
+'''Data'''
+data_box = Listbox(DataFrame, width=100, height=18, yscrollcommand=sb.set, font=("Calibri", 14, "bold"))
+sb.config(command=data_box.yview)
+
+
+# Naam = Label(DataFrame, text="Naam", bg="yellow", fg="black", width=12, font=("Calibri", 16, "bold"))
+# Genre = Label(DataFrame, text="Genre", bg="yellow", fg="black", width=12, font=("Calibri", 16, "bold"))
+# ReleaseDate = Label(DataFrame, text="Release date", bg="yellow", fg="black", width=12, font=("Calibri", 16, "bold"))
+# Price = Label(DataFrame, text="Price in $", bg="yellow", fg="black", width=12, font=("Calibri", 16, "bold"))
+# Owners = Label(DataFrame, text="Owners", bg="yellow", fg="black", width=12, font=("Calibri", 16, "bold"))
+# Rating = Label(DataFrame, text="Rating in %", bg="yellow", fg="black", width=12, font=("Calibri", 16, "bold"))
+# RequiredAge = Label(DataFrame, text="Legal age", bg="yellow", fg="black", width=12, font=("Calibri", 16, "bold"))
+#
+# Naam.grid(row=0, column=0, pady=5)
+# Genre.grid(row=1, column=0, pady=5)
+# ReleaseDate.grid(row=2, column=0, pady=5)
+# Price.grid(row=3, column=0, pady=5)
+# Owners.grid(row=4, column=0, pady=5)
+# Rating.grid(row=5, column=0, pady=5)
+# RequiredAge.grid(row=6, column=0, pady=5)
+
+
+toon_spellen()
 root.mainloop()
