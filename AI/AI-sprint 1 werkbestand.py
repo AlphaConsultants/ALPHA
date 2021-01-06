953ab5f48cf9fd92cd5e343c51369fedb7a79a64
Abdullah03366 <abdullah.sanli@student.hu.nl>
2021-01-06 17:09:12 +0100

    werkbestand

---
+++ AI/AI-sprint 1 werkbestand.py
@@ -0,0 +1,247 @@
+from tkinter import *
+import requests
+
+github_example = "https://raw.githubusercontent.com/AlphaConsultants/ALPHA/main/AI/textdocumenttest.json"
+
+json_data = requests.get(github_example).json()
+
+games_list = []
+
+
+def datascherm1_tonen():
+    Hoofdscherm.pack_forget()
+    Datascherm2.pack_forget()
+    TerugknopFrame.pack_forget()
+    TerugknopFrame.pack(pady=20)
+    Datascherm.pack()
+    Datascherm1.pack()
+    Terug.pack()
+
+
+def datascherm2_tonen():
+    Hoofdscherm.pack_forget()
+    TerugknopFrame.pack_forget()
+    Datascherm1.pack_forget()
+    TerugknopFrame.pack(pady=20)
+    Datascherm.pack()
+    Datascherm2.pack()
+    Terug.pack()
+
+
+def hoofdscherm_tonen():
+    Datascherm.pack_forget()
+    TerugknopFrame.pack_forget()
+    Hoofdscherm.pack()
+
+
+def data_opvragen():
+    games_list.clear()
+    for data in json_data:
+        rating_in_percentage = ((data["positive_ratings"] - data["negative_ratings"]) / data["positive_ratings"]) * 100
+        rating_in_percentage_afgerond = str(round(rating_in_percentage, 1))
+        data_per_game = data["name"], data["genres"], data["release_date"], data["price"], data[
+            "owners"], rating_in_percentage_afgerond, data["required_age"]
+        games_list.append(tuple(data_per_game))
+
+
+def informatie_tonen():
+    datascherm1_tonen()
+    total_rows = len(games_list)
+    total_columns = len(games_list[0])
+    for games in range(total_rows):
+        for game in range(total_columns):
+            e = Entry(Datascherm1, fg='blue', font=('Arial', 12, 'bold'))
+            e.grid(row=games, column=game)
+            e.insert(END, games_list[games][game])
+
+
+def toon_spellen():
+    data_opvragen()
+    informatie_tonen()
+
+
+def toon_eerste_spel():
+    data_opvragen()
+    datascherm2_tonen()
+    a = games_list[0][0]
+    e = Entry(Datascherm2, fg='blue', font=('Arial', 12, 'bold'))
+    e.grid(row=0, column=0)
+    e.insert(END, a)
+
+
+def toon_naam_az():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[0])
+    informatie_tonen()
+
+
+def toon_naam_za():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[0], reverse=True)
+    informatie_tonen()
+
+
+def toon_genre_az():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[1])
+    informatie_tonen()
+
+
+def toon_genre_za():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[1], reverse=True)
+    informatie_tonen()
+
+
+def toon_release_date_on():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[2])
+    informatie_tonen()
+
+
+def toon_release_date_no():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[2], reverse=True)
+    informatie_tonen()
+
+
+def toon_prijs_lh():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[3])
+    informatie_tonen()
+
+
+def toon_prijs_hl():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[3], reverse=True)
+    informatie_tonen()
+
+
+def toon_owners_hl():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[4])
+    informatie_tonen()
+
+
+def toon_owners_lh():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[4], reverse=True)
+    informatie_tonen()
+
+
+def toon_rating_lh():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[5])
+    informatie_tonen()
+
+
+def toon_rating_hl():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[5], reverse=True)
+    informatie_tonen()
+
+
+def toon_required_age_lh():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[6])
+    informatie_tonen()
+
+
+def toon_required_age_hl():
+    data_opvragen()
+    games_list.sort(key=lambda tup: tup[6], reverse=True)
+    informatie_tonen()
+
+
+root = Tk()
+root.title("Dashboard Steam")
+root.geometry("1500x1000")
+root.configure(bg="#17202e")
+
+Hoofdscherm = Frame(root)
+Datascherm = Frame(root)
+TerugknopFrame = Frame(root)
+Datascherm1 = Frame(Datascherm)
+Datascherm2 = Frame(Datascherm)
+
+'''Titel van dashboard'''
+titel = Label(root, text="Alpha Consultants", bg="#17202e", fg="white", font=("Calibri", 40, "bold", "underline"))
+titel.pack(pady=90)
+
+'''Frames voor widgets'''
+SorteerButtonFrame = Frame(Hoofdscherm, bg="#17202e")
+LinkerFrame = Frame(SorteerButtonFrame, bg="#17202e")
+RechterFrame = Frame(SorteerButtonFrame, bg="#17202e")
+
+Hoofdscherm.pack()
+SorteerButtonFrame.pack(side=RIGHT)
+LinkerFrame.pack(side=LEFT, pady=55, padx=30)
+RechterFrame.pack(side=RIGHT, pady=55, padx=30)
+
+'''Sorteerknoppen met titel op hoofdscherm'''
+SorteerTitel = Label(SorteerButtonFrame, text="Sorteren op:", bg="#17202e", fg="white", font=("Calibri", 24, "bold"))
+ToonAlleGames = Button(RechterFrame, text="Toon Games", width=24, font=("Calibri", 14, "bold"), command=toon_spellen)
+Eerste_Spel = Button(LinkerFrame, text="Eerste Spel", width=24, font=("Calibri", 14, "bold"), command=toon_eerste_spel)
+Name_AZ = Button(LinkerFrame, text="Naam (A-Z)", width=24, font=("Calibri", 14, "bold"), command=toon_naam_az)
+Name_ZA = Button(RechterFrame, text="Naam (Z-A)", width=24, font=("Calibri", 14, "bold"), command=toon_naam_za)
+Genre_AZ = Button(LinkerFrame, text="Genre (A-Z)", width=24, font=("Calibri", 14, "bold"), command=toon_genre_az)
+Genre_ZA = Button(RechterFrame, text="Genre (Z-A)", width=24, font=("Calibri", 14, "bold"), command=toon_genre_za)
+ReleaseDate_ON = Button(LinkerFrame, text="Releasedatum (oud-nieuw)", width=24, font=("Calibri", 14, "bold"),
+                        command=toon_release_date_on)
+ReleaseDate_NO = Button(RechterFrame, text="Releasedatum (nieuw-oud)", width=24, font=("Calibri", 14, "bold"),
+                        command=toon_release_date_no)
+Price_LH = Button(LinkerFrame, text="Prijs (laag-hoog)", width=24, font=("Calibri", 14, "bold"), command=toon_prijs_lh)
+Price_HL = Button(RechterFrame, text="Prijs (hoog-laag)", width=24, font=("Calibri", 14, "bold"), command=toon_prijs_hl)
+Owners_LH = Button(LinkerFrame, text="Owners (laag-hoog)", width=24, font=("Calibri", 14, "bold"),
+                   command=toon_owners_lh)
+Owners_HL = Button(RechterFrame, text="Owners (hoog-laag)", width=24, font=("Calibri", 14, "bold"),
+                   command=toon_owners_hl)
+Rating_LH = Button(LinkerFrame, text="Rating (laag-hoog)", width=24, font=("Calibri", 14, "bold"),
+                   command=toon_rating_lh)
+Rating_HL = Button(RechterFrame, text="Rating (hoog-laag)", width=24, font=("Calibri", 14, "bold"),
+                   command=toon_rating_hl)
+RequiredAge_LH = Button(LinkerFrame, text="Required Age (laag-hoog)", width=24, font=("Calibri", 14, "bold"),
+                        command=toon_required_age_lh)
+RequiredAge_HL = Button(RechterFrame, text="Required Age (hoog-laag)", width=24, font=("Calibri", 14, "bold"),
+                        command=toon_required_age_hl)
+
+''''Terugknop van Datascherm naar Hoofdscherm'''
+Terug = Button(TerugknopFrame, text="Terug naar Hoofdscherm", font=("Calibri", 14, "bold"),
+               command=hoofdscherm_tonen)
+
+NameLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
+GenreLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
+DateLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
+PriceLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
+OwnersLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
+RatingLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
+RequiredAgeLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
+
+SorteerTitel.place(x=30, y=10)
+Eerste_Spel.pack(pady=10)
+ToonAlleGames.pack(pady=10)
+Name_AZ.pack(pady=10)
+Name_ZA.pack(pady=10)
+Genre_AZ.pack(pady=10)
+Genre_ZA.pack(pady=10)
+ReleaseDate_ON.pack(pady=10)
+ReleaseDate_NO.pack(pady=10)
+Price_LH.pack(pady=10)
+Price_HL.pack(pady=10)
+Owners_LH.pack(pady=10)
+Owners_HL.pack(pady=10)
+Rating_LH.pack(pady=10)
+Rating_HL.pack(pady=10)
+RequiredAge_LH.pack(pady=10)
+RequiredAge_HL.pack(pady=10)
+
+# '''Scrollbar'''
+# sb = Scrollbar(Datascherm)
+# sb.pack(side=RIGHT, fill=Y)
+#
+# '''Data'''
+# data_box = Listbox(DataFrame, width=75, height=18, yscrollcommand=sb.set, font=("Calibri", 14, "bold"))
+# sb.config(command=Datascherm.yview)
+
+hoofdscherm_tonen()
+root.mainloop()
