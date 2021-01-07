ee5443531885d1ddbe2e8d22d5eee2db73b3cd1c
Abdullah03366 <abdullah.sanli@student.hu.nl>
2021-01-07 02:27:42 +0100

    eigen sorteerfunctie gemaakt

--- AI/AI final werkbestand.py
+++ AI/AI final werkbestand.py
@@ -7,6 +7,95 @@ json_data = requests.get(github_example).json()
 
 games_list = []
 
+def sorteren(keuze):
+    '''Sorteren op:
+    -   keuze 0 = Naam A-Z
+    -   keuze 1 = Naam Z-A
+    -   keuze 2 = Genre A-Z
+    -   keuze 3 = Genre Z-A
+    -   keuze 4 = Releasedatum Oud - Nieuw
+    -   keuze 5 = Releasedatum Nieuw - Oud
+    -   keuze 6 = Prijs Laag - Hoog
+    -   keuze 7 = Prijs Hoog - Laag
+    -   keuze 8 = Owners Laag - Hoog
+    -   keuze 9 = Owners Hoog - Laag
+    -   keuze 10 = Rating Laag - Hoog
+    -   keuze 11 = Rating Hoog - Laag'''
+
+    n = len(games_list)
+    if keuze == 0:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][0] > games_list[g+1][0]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 1:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][0] < games_list[g+1][0]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 2:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][1] > games_list[g+1][1]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 3:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][1] < games_list[g+1][1]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 4:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][2] > games_list[g+1][2]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 5:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][2] < games_list[g+1][2]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 6:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][3] > games_list[g+1][3]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 7:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][3] < games_list[g+1][3]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 8:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][4] > games_list[g+1][4]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 9:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][4] < games_list[g+1][4]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 10:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][5] > games_list[g+1][5]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+    elif keuze == 11:
+        for index in range(n):
+            for g in range(0, n - index - 1):
+                if games_list[g][5] < games_list[g+1][5]:
+                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
+        return games_list
+
 
 def datascherm1_tonen():
     Hoofdscherm.pack_forget()
@@ -71,73 +160,73 @@ def toon_eerste_spel():
 
 def toon_naam_az():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[0])
+    sorteren(0)
     informatie_tonen()
 
 
 def toon_naam_za():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[0], reverse=True)
+    sorteren(1)
     informatie_tonen()
 
 
 def toon_genre_az():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[1])
+    sorteren(2)
     informatie_tonen()
 
 
 def toon_genre_za():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[1], reverse=True)
+    sorteren(3)
     informatie_tonen()
 
 
 def toon_release_date_on():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[2])
+    sorteren(4)
     informatie_tonen()
 
 
 def toon_release_date_no():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[2], reverse=True)
+    sorteren(5)
     informatie_tonen()
 
 
 def toon_prijs_lh():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[3])
+    sorteren(6)
     informatie_tonen()
 
 
 def toon_prijs_hl():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[3], reverse=True)
+    sorteren(7)
     informatie_tonen()
 
 
 def toon_owners_hl():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[4])
+    sorteren(8)
     informatie_tonen()
 
 
 def toon_owners_lh():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[4], reverse=True)
+    sorteren(9)
     informatie_tonen()
 
 
 def toon_rating_lh():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[5])
+    sorteren(10)
     informatie_tonen()
 
 
 def toon_rating_hl():
     data_opvragen()
-    games_list.sort(key=lambda tup: tup[5], reverse=True)
+    sorteren(11)
     informatie_tonen()
 
 
@@ -146,25 +235,30 @@ root.title("Dashboard Steam")
 root.geometry("1500x1000")
 root.configure(bg="#17202e")
 
+
 Hoofdscherm = Frame(root)
 Datascherm = Frame(root)
 TerugknopFrame = Frame(root)
 Datascherm1 = Frame(Datascherm)
 Datascherm2 = Frame(Datascherm)
 
+
 '''Titel van dashboard'''
 titel = Label(root, text="Alpha Consultants", bg="#17202e", fg="white", font=("Calibri", 40, "bold", "underline"))
 titel.pack(pady=90)
+
 
 '''Frames voor widgets'''
 SorteerButtonFrame = Frame(Hoofdscherm, bg="#17202e")
 LinkerFrame = Frame(SorteerButtonFrame, bg="#17202e")
 RechterFrame = Frame(SorteerButtonFrame, bg="#17202e")
 
+
 Hoofdscherm.pack()
 SorteerButtonFrame.pack(side=RIGHT)
 LinkerFrame.pack(side=LEFT, pady=55, padx=30)
 RechterFrame.pack(side=RIGHT, pady=55, padx=30)
+
 
 '''Sorteerknoppen met titel op hoofdscherm'''
 SorteerTitel = Label(SorteerButtonFrame, text="Sorteren op:", bg="#17202e", fg="white", font=("Calibri", 24, "bold"))
@@ -194,6 +288,7 @@ Rating_HL = Button(RechterFrame, text="Rating (hoog-laag)", width=24, font=("Cal
 Terug = Button(TerugknopFrame, text="Terug naar Hoofdscherm", font=("Calibri", 14, "bold"),
                command=hoofdscherm_tonen)
 
+
 NameLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
 GenreLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
 DateLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
@@ -201,6 +296,7 @@ PriceLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bol
 OwnersLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
 RatingLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
 RequiredAgeLabel = Entry(Datascherm1, text="AppID", fg='blue', font=('Arial', 12, 'bold'))
+
 
 SorteerTitel.place(x=30, y=10)
 Eerste_Spel.pack(pady=10)
@@ -218,13 +314,6 @@ Owners_HL.pack(pady=10)
 Rating_LH.pack(pady=10)
 Rating_HL.pack(pady=10)
 
-# '''Scrollbar'''
-# sb = Scrollbar(Datascherm)
-# sb.pack(side=RIGHT, fill=Y)
-#
-# '''Data'''
-# data_box = Listbox(DataFrame, width=75, height=18, yscrollcommand=sb.set, font=("Calibri", 14, "bold"))
-# sb.config(command=Datascherm.yview)
 
 hoofdscherm_tonen()
 root.mainloop()
