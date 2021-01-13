d9a47c36861c536cee2b5571eac8d701856700e5
Abdullah03366 <abdullah.sanli@student.hu.nl>
2021-01-13 01:53:02 +0100

    JAJAAA de bugs zijn weggewerkt en de code werkt nu naar behoren. Entry x Grid is veranderd naar Tkinter Treeview.

--- AI/AI final definitief.py
+++ AI/AI final definitief.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter.messagebox import showinfo
 import requests
+
 
 """Data opvragen van onderstaande link d.m.v. requests.get"""
 github_example = "https://raw.githubusercontent.com/AlphaConsultants/ALPHA/main/AI/textdocumenttest.json"
 json_data = requests.get(github_example).json()
+
 
 games_list = []
 
@@ -13,14 +16,28 @@ def data_in_datastructuur():
     """Functie haalt nuttige info uit json en zet het in games_list als tuples. games_list = [(),(),()]"""
     games_list.clear()
     for data in json_data:
+        upper_name = data["name"].upper()
+        upper_genre = data["genres"].upper()
         rating_in_percentage = ((data["positive_ratings"] - data["negative_ratings"]) / data["positive_ratings"]) * 100
         rating_in_percentage_afgerond = str(round(rating_in_percentage, 1))
-        data_per_game = data["name"], data["genres"], data["release_date"], data["price"], data[
-            "owners"], rating_in_percentage_afgerond
+        data_per_game = upper_name, upper_genre, data["release_date"], data["price"], rating_in_percentage_afgerond
         games_list.append(tuple(data_per_game))
 
 
-def bubblesort(choice, reverse):
+def data_in_tabel(lst):
+    """Functie haalt gegevens uit de list en voegt ze vervolgens als gegevens toe aan de tabel"""
+    for rij in range(len(lst)):
+        Tabel.insert(parent="", index="end", iid=rij, values=lst[rij])
+    GesorteerdeDataScherm1.pack()
+
+
+def verwijder_tabel_gegevens():
+    """Functie verwijdert de gegevens van de tabel."""
+    for data in Tabel.get_children():
+        Tabel.delete(data)
+
+
+def bubblesort(lst, choice, reverse):
     """Functie sorteert opgehaalde data
         Sorteren op:
     -   reverse 0 = A-Z, Oud-Nieuw, Laag-Hoog
@@ -29,21 +46,21 @@ def bubblesort(choice, reverse):
     -   choice 1 = Genre
     -   choice 2 = Releasedatum
     -   choice 3 = Prijs
-    -   choice 4 = Owners
-    -   choice 5 = Rating"""
-    n = len(games_list)
+    -   choice 4 = Rating
+    """
+    n = len(lst)
     if reverse == 0:
         for index in range(n):
             for g in range(0, n - index - 1):
-                if games_list[g][choice] > games_list[g+1][choice]:
-                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
-        return games_list
+                if lst[g][choice] > lst[g+1][choice]:
+                    lst[g], lst[g+1] = lst[g+1], lst[g]
+        return lst
     elif reverse == 1:
         for index in range(n):
             for g in range(0, n - index - 1):
-                if games_list[g][choice] < games_list[g+1][choice]:
-                    games_list[g], games_list[g+1] = games_list[g+1], games_list[g]
-        return games_list
+                if lst[g][choice] < lst[g+1][choice]:
+                    lst[g], lst[g+1] = lst[g+1], lst[g]
+        return lst
 
 
 def beginscherm_tonen():
@@ -54,7 +71,8 @@ def beginscherm_tonen():
     GesorteerdeDataScherm.pack_forget()
     GesorteerdeDataScherm1.pack_forget()
     ZoekenInDataScherm.pack_forget()
-    ZoekenInDataScherm1.pack_forget()
+    ResultaatScherm.pack_forget()
+    verwijder_tabel_gegevens()
     BeginScherm.pack()
 
 
@@ -63,14 +81,7 @@ def ongesorteerde_datascherm_tonen():
     BeginScherm.pack_forget()
     OngesorteerdeDataScherm.pack()
     data_in_datastructuur()
-    GesorteerdeDataScherm1.pack()
-    total_rows = len(games_list)
-    total_columns = len(games_list[0])
-    for games in range(total_rows):
-        for game in range(total_columns):
-            e = Entry(GesorteerdeDataScherm1, fg='blue', font=('Arial', 12, 'bold'), width=18)
-            e.grid(row=games, column=game)
-            e.insert(END, games_list[games][game])
+    data_in_tabel(games_list)
 
 
 def sorteeroptiesscherm_tonen():
@@ -78,30 +89,28 @@ def sorteeroptiesscherm_tonen():
     BeginScherm.pack_forget()
     GesorteerdeDataScherm.pack_forget()
     GesorteerdeDataScherm1.pack_forget()
-    GesorteerdeDataScherm2.pack_forget()
     SorteerButtonsScherm.pack()
     SorteerButtonsScherm1.pack()
+    data_in_datastructuur()
+    verwijder_tabel_gegevens()
 
 
 def gesorteerde_datacherm_tonen():
     """Functie toont gesorteerde datascherm van GUI"""
     SorteerButtonsScherm.pack_forget()
     SorteerButtonsScherm1.pack_forget()
-    GesorteerdeDataScherm2.pack_forget()
     GesorteerdeDataScherm.pack()
     GesorteerdeDataScherm1.pack()
-    total_rows = len(games_list)
-    total_columns = len(games_list[0])
-    for games in range(total_rows):
-        for game in range(total_columns):
-            e = Entry(GesorteerdeDataScherm1, fg='blue', font=('Arial', 12, 'bold'), width=18)
-            e.grid(row=games, column=game)
-            e.insert(END, games_list[games][game])
+    data_in_tabel(games_list)
 
 
 def zoeken_in_datascherm_tonen():
     """Functie toont zoeken in datascherm van GUI"""
+    verwijder_tabel_gegevens()
     BeginScherm.pack_forget()
+    GesorteerdeDataScherm1.pack_forget()
+    ResultaatScherm.pack_forget()
+    data_in_datastructuur()
     Zoekvak.delete(0, "end")
     ZoekenInDataScherm.pack()
 
@@ -113,43 +122,35 @@ def zoeken(choice, target):
     -   choice 1 = Genre"""
     games_list2 = []
     if not target:
-        ZoekenInDataScherm1.pack_forget()
         waarschuwing = "Voer eerst een zoekopdracht in."
         showinfo(title='popup', message=waarschuwing)
-        ZoekenInDataScherm1.destroy()
     else:
-        games_list2.clear()
         for char in games_list:
             if target in char[choice]:
                 games_list2.append(tuple(char))
         if not games_list2:
             bericht2 = 'Uw zoekopdracht komt niet voor in de lijst.'
             showinfo(title='popup', message=bericht2)
-            ZoekenInDataScherm1.pack_forget()
             Zoekvak.delete(0, "end")
         else:
             bericht = 'Uw zoekopdracht komt voor in de lijst.'
             showinfo(title='popup', message=bericht)
             Zoekvak.delete(0, "end")
-            total_rows = len(games_list2)
-            total_columns = len(games_list2[0])
-            for games in range(total_rows):
-                for game in range(total_columns):
-                    e = Entry(ZoekenInDataScherm1, fg='blue', font=('Arial', 12, 'bold'), width=18)
-                    e.grid(row=games, column=game)
-                    e.insert(END, games_list2[games][game])
-                ZoekenInDataScherm1.pack()
+            ZoekenInDataScherm.pack_forget()
+            ResultaatScherm.pack()
+            VanResultaatSchermNaarZoekenInDataScherm.pack(pady=50)
+            data_in_tabel(games_list2)
 
 
 def naam_clicked():
     """Functie voert zoeken(choice, target) uit na geklik"""
-    zoekopdracht = Zoekvak.get()
+    zoekopdracht = Zoekvak.get().upper()
     zoeken(0, zoekopdracht)
 
 
 def genre_clicked():
     """Functie voert zoeken(choice, target) uit na geklik"""
-    zoekopdracht = Zoekvak.get()
+    zoekopdracht = Zoekvak.get().upper()
     zoeken(1, zoekopdracht)
 
 
@@ -157,99 +158,84 @@ def eerste_spel_in_gesorteerdescherm_tonen():
     """Functie toont eerste spel van ongesorteerde lijst"""
     SorteerButtonsScherm.pack_forget()
     SorteerButtonsScherm1.pack_forget()
-    GesorteerdeDataScherm1.pack_forget()
     GesorteerdeDataScherm.pack()
-    GesorteerdeDataScherm2.pack()
+    GesorteerdeDataScherm1.pack()
     data_in_datastructuur()
     GesorteerdTitel["text"] = "Eerste spel in ongesorteerde lijst"
-    a = games_list[0][0]
-    e = Entry(GesorteerdeDataScherm2, fg='blue', font=('Arial', 12, 'bold'))
-    e.grid(row=0, column=0)
-    e.insert(END, a)
+    Tabel.insert(parent="", index="end", iid=0, text="", values=games_list[0])
 
 
 def toon_naam_az():
     """Functie toont tabel gesorteerd op Naam"""
-    bubblesort(0, 0)
+    bubblesort(games_list, 0, 0)
     GesorteerdTitel["text"] = "Gesorteerd op Naam A-Z"
     gesorteerde_datacherm_tonen()
 
 
 def toon_naam_za():
     """Functie toont tabel gesorteerd op Naam"""
-    bubblesort(0, 1)
+    bubblesort(games_list, 0, 1)
     GesorteerdTitel["text"] = "Gesorteerd op Naam Z-A"
     gesorteerde_datacherm_tonen()
 
 
 def toon_genre_az():
     """Functie toont tabel gesorteerd op Genre"""
-    bubblesort(1, 0)
+    bubblesort(games_list, 1, 0)
     GesorteerdTitel["text"] = "Gesorteerd op Genre A-Z"
     gesorteerde_datacherm_tonen()
 
 
 def toon_genre_za():
     """Functie toont tabel gesorteerd op Genre"""
-    bubblesort(1, 1)
+    bubblesort(games_list, 1, 1)
     GesorteerdTitel["text"] = "Gesorteerd op Genre Z-A"
     gesorteerde_datacherm_tonen()
 
 
 def toon_release_date_on():
     """Functie toont tabel gesorteerd op Releasedate"""
-    bubblesort(2, 0)
+    bubblesort(games_list, 2, 0)
     GesorteerdTitel["text"] = "Gesorteerd op Releasedatum Oud-Nieuw"
     gesorteerde_datacherm_tonen()
 
 
 def toon_release_date_no():
     """Functie toont tabel gesorteerd op Releasedate"""
-    bubblesort(2, 1)
+    bubblesort(games_list, 2, 1)
     GesorteerdTitel["text"] = "Gesorteerd op Releasedatum Nieuw-Oud"
     gesorteerde_datacherm_tonen()
 
 
 def toon_prijs_lh():
     """Functie toont tabel gesorteerd op Prijs"""
-    bubblesort(3, 0)
+    bubblesort(games_list, 3, 0)
     GesorteerdTitel["text"] = "Gesorteerd op Prijs Laag-Hoog"
     gesorteerde_datacherm_tonen()
 
 
 def toon_prijs_hl():
     """Functie toont tabel gesorteerd op Prijs"""
-    bubblesort(3, 1)
+    bubblesort(games_list, 3, 1)
     GesorteerdTitel["text"] = "Gesorteerd op Prijs Hoog-Laag"
-    gesorteerde_datacherm_tonen()
-
-
-def toon_owners_hl():
-    """Functie toont tabel gesorteerd op Owners"""
-    bubblesort(4, 0)
-    GesorteerdTitel["text"] = "Gesorteerd op Owners Laag-Hoog"
-    gesorteerde_datacherm_tonen()
-
-
-def toon_owners_lh():
-    """Functie toont tabel gesorteerd op Owners"""
-    bubblesort(4, 1)
-    GesorteerdTitel["text"] = "Gesorteerd op Owners Hoog-Laag"
     gesorteerde_datacherm_tonen()
 
 
 def toon_rating_lh():
     """Functie toont tabel gesorteerd op Rating"""
-    bubblesort(5, 0)
+    bubblesort(games_list, 4, 0)
     GesorteerdTitel["text"] = "Rating (laag-hoog)"
     gesorteerde_datacherm_tonen()
 
 
 def toon_rating_hl():
     """Functie toont tabel gesorteerd op Rating"""
-    bubblesort(5, 1)
+    bubblesort(games_list, 4, 1)
     GesorteerdTitel["text"] = "Rating (hoog-laag)"
     gesorteerde_datacherm_tonen()
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~GUI~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 root = Tk()
@@ -272,11 +258,12 @@ SorteerButtonsScherm1 = Frame(root, bg="#17202e")
 """Frame van gesorteerde datascherm"""
 GesorteerdeDataScherm = Frame(root, bg="#17202e")
 GesorteerdeDataScherm1 = Frame(root, bg="#17202e")
-GesorteerdeDataScherm2 = Frame(root, bg="#17202e")
 
 """Frame van zoeken in datascherm"""
 ZoekenInDataScherm = Frame(root, bg="#17202e")
-ZoekenInDataScherm1 = Frame(root, bg="#17202e")
+
+"""Frame van resultaat na het zoeken"""
+ResultaatScherm = Frame(root, bg="#17202e")
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FRAME-BEGINSCHERM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -285,10 +272,27 @@ BeginschermTitel = Label(BeginScherm, text="Alpha Consultants", bg="#17202e", fg
 keuze = Label(BeginScherm, text="Maak een keuze:", bg="#17202e", fg="white", font=("Calibri", 22, "bold"))
 
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~FRAME-GESORTEERDE DATASCHERM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~FRAME-ONGESORTEERDE DATASCHERM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """Titel van ongesorteerde scherm GUI"""
 OngesorteerdTitel = Label(OngesorteerdeDataScherm, text="Data ongesorteerd:", bg="#17202e", fg="white", font=("Calibri", 22, "bold"))
+Tabel = ttk.Treeview(GesorteerdeDataScherm1)
 
+Tabel["columns"] = ("Name", "Genre", "Releasedate", "Price", "Rating in %")
+
+Tabel.column("#0", width=0, minwidth=0)
+Tabel.column("Name", anchor=W, width=160)
+Tabel.column("Genre", anchor=CENTER, width=120)
+Tabel.column("Releasedate", anchor=CENTER, width=120)
+Tabel.column("Price", anchor=CENTER, width=80)
+Tabel.column("Rating in %", anchor=CENTER, width=100)
+
+
+Tabel.heading("#0", text="", anchor=W)
+Tabel.heading("Name", text="Name", anchor=W)
+Tabel.heading("Genre", text="Genre", anchor=CENTER)
+Tabel.heading("Releasedate", text="Releasedate", anchor=CENTER)
+Tabel.heading("Price", text="Price", anchor=CENTER)
+Tabel.heading("Rating in %", text="Rating in %", anchor=CENTER)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FRAME-SORTEEROPTIES SCHERM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """Titel van Sorteeropties scherm en buttons voor het sorteren"""
@@ -305,10 +309,6 @@ ReleaseDate_NO = Button(SorteerButtonsScherm1, text="Releasedatum (nieuw-oud)", 
                         command=toon_release_date_no)
 Price_LH = Button(SorteerButtonsScherm1, text="Prijs (laag-hoog)", width=24, font=("Calibri", 14, "bold"), command=toon_prijs_lh)
 Price_HL = Button(SorteerButtonsScherm1, text="Prijs (hoog-laag)", width=24, font=("Calibri", 14, "bold"), command=toon_prijs_hl)
-Owners_LH = Button(SorteerButtonsScherm1, text="Owners (laag-hoog)", width=24, font=("Calibri", 14, "bold"),
-                   command=toon_owners_lh)
-Owners_HL = Button(SorteerButtonsScherm1, text="Owners (hoog-laag)", width=24, font=("Calibri", 14, "bold"),
-                   command=toon_owners_hl)
 Rating_LH = Button(SorteerButtonsScherm1, text="Rating (laag-hoog)", width=24, font=("Calibri", 14, "bold"),
                    command=toon_rating_lh)
 Rating_HL = Button(SorteerButtonsScherm1, text="Rating (hoog-laag)", width=24, font=("Calibri", 14, "bold"),
@@ -345,7 +345,8 @@ ZoekenInDataButton = Button(BeginScherm, text="Zoeken in Data", width=30, font=(
                             command=zoeken_in_datascherm_tonen)
 VanZoekenInDataSchermNaarBeginscherm = Button(ZoekenInDataScherm, text="Terug", font=("Calibri", 14, "bold"),
                                               command=beginscherm_tonen)
-
+VanResultaatSchermNaarZoekenInDataScherm = Button(ResultaatScherm, text="Terug", font=("Calibri", 14, "bold"),
+                                              command=zoeken_in_datascherm_tonen)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~POSITIES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """Posities van widgets in Beginscherm"""
@@ -358,6 +359,7 @@ ZoekenInDataButton.pack(pady=20)
 """Posities van widgets in OngesorteerdeDataScherm"""
 VanOngesorteerdeDataSchermNaarBeginscherm.pack(pady=50)
 OngesorteerdTitel.pack(pady=20)
+Tabel.pack(pady=20)
 
 """Posities van widgets in SorteerButtonsScherm"""
 VanSorteerButtonsSchermNaarBeginscherm.pack(pady=50)
@@ -372,8 +374,6 @@ ReleaseDate_ON.grid(row=2, column=0)
 ReleaseDate_NO.grid(row=2, column=1)
 Price_LH.grid(row=3, column=0)
 Price_HL.grid(row=3, column=1)
-Owners_LH.grid(row=4, column=0)
-Owners_HL.grid(row=4, column=1)
 Rating_LH.grid(row=5, column=0)
 Rating_HL.grid(row=5, column=1)
 
@@ -389,6 +389,5 @@ NameButton.pack()
 GenreButton.pack()
 
 
-data_in_datastructuur()
 beginscherm_tonen()
 root.mainloop()
